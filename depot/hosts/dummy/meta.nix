11402e45e51a6a7ce855c1f2e3eea3ef8663de99
kat witch <kat@kittywit.ch>
2021-08-17 22:49:03 +0100

    replace host import

--- depot/hosts/dummy/meta.nix
+++ depot/hosts/dummy/meta.nix
@@ -1,9 +1,11 @@
 { lib, config, root,  profiles, ... }: with lib; {
   deploy.targets.dummy.enable = false;
   network.nodes.dummy = {
-    imports = lib.hostImport {
-      hostName = "dummy";
-      inherit profiles root;
+    imports = filter builtins.pathExists singleton ./nixos.nix
+    ++ (if builtins.Attrs profiles.base then profiles.base.imports else singleton profiles.base) ++ singleton {
+      home-manager.users.example = {
+        imports = filter builtins.pathExists singleton ./home.nix;
+      };
     };
     networking = {
       hostName = "dummy";
--- depot/hosts/example/meta.nix
+++ depot/hosts/example/meta.nix
@@ -13,9 +13,11 @@
       };
     };
     network.nodes.example = {
-      imports = lib.hostImport {
-        hostName = "example";
-        inherit profiles root;
+      imports = filter builtins.pathExists singleton ./nixos.nix
+      ++ (if builtins.Attrs profiles.base then profiles.base.imports else singleton profiles.base) ++ singleton {
+        home-manager.users.example = {
+          imports = filter builtins.pathExists singleton ./home.nix;
+        };
       };
     };
   };
