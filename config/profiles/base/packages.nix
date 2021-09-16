15e7116173f116330da0c36ed273d42de9dd16b8
Glowpelt <glowpelt@chakat.space>
2021-09-15 19:00:12 -0600

    base: Add terminfo for likely terminals

--- config/profiles/base/packages.nix
+++ config/profiles/base/packages.nix
@@ -2,9 +2,13 @@
 
 {
   environment.systemPackages = with pkgs; [
+    neofetch
     smartmontools
     hddtemp
     lm_sensors
     cachix
+    # Terminfo for some terminals, so even over SSH things work fine
+    kitty.terminfo
+    foot.terminfo
   ];
 }
