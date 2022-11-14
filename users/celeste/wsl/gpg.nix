2579f85fd9517ad2d26a99f05b0f7791dcfeb609
Celeste <celeste@foxineering.work>
2022-11-13 22:20:16 -0700

    more wsl gpg fixes
    this absolutely is not enough to get it to behave even half the time
    a little bit at a time i guess, but...

--- users/celeste/wsl/gpg.nix
+++ users/celeste/wsl/gpg.nix
@@ -5,20 +5,23 @@
 {
   # Disable the normal gpg-agent
   services.gpg-agent.enable = mkForce false;
+  # Don't autostart it ever either
+  programs.gpg.settings.no-autostart = true;
   # Fish config
   programs.fish.shellInit = let
     ss = pkgs.iproute + "/bin/ss";
     grep = pkgs.gnugrep + "/bin/grep";
     socat = pkgs.socat + "/bin/socat";
     windows_dest = "/mnt/c/nixos/wsl2-ssh-pageant.exe";
+    homedir = config.programs.gpg.homedir;
   in ''
     set -x SSH_AUTH_SOCK "$HOME/.ssh/agent.sock"
-    set -x GPG_AGENT_SOCK "/run/user/1000/gnupg/S.gpg-agent"
+    set -x GPG_AGENT_SOCK "${homedir}/S.gpg-agent"
     if not ${ss} -a | ${grep} -q "$SSH_AUTH_SOCK";
       rm -f "$SSH_AUTH_SOCK"
       rm -f "$GPG_AGENT_SOCK"
-      mkdir -p /run/user/1000/gnupg
-      if not test -x "${windows_dest}";
+      mkdir -p "${homedir}"
+      if not test -x "${windows_dest}"; # TODO: always do? check hash? it's fine for me personally, but technically someone could modify the exe out from under us
         install -Dm 755 -T "${exe}" "${windows_dest}"
       end
       setsid -f nohup ${socat} UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"${windows_dest}" >/dev/null 2>&1 &
