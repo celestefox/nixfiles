7cda5ad2c166b72c025a362a91c1c536ce882e24
Celeste <celeste@foxineering.work>
2023-01-12 13:22:15 -0700

    bottles doesn't take external wine anymore, mc changes

--- users/celeste/gui/gaymes.nix
+++ users/celeste/gui/gaymes.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: with lib; let
   myWine = pkgs.wineWowPackages.full;
-  myBottles = pkgs.bottles.override { wineWowPackages = { minimal = myWine; }; };
+  #myBottles = pkgs.bottles.override { wineWowPackages = { minimal = myWine; }; };
 in
 {
   home.packages = with pkgs; [
@@ -9,11 +9,12 @@ in
     #lutris # some game manager i guess prolly
     wyvern # GOG games downloader/installer handler
     myWine #wine
-    myBottles # alternative wineprefix manager/incl own deps instead of winetricks
+    bottles # alternative wineprefix manager/incl own deps instead of winetricks
     winetricks # fancy wine stuff
     protontricks # winetricks for proton
     #minecraft # the normal launcher, ehh
-    polymc # multimc fork
+    #polymc # multimc fork
+    prismlauncher # PolyMC fork after takeover
     minetest # neat game
     ccemux # computercraft emulator
     #runelite # OSRS client, ehh
