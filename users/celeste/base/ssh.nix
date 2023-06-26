ad6a8fc3e2fb93095ef5e5077397b7359237030e
Celeste <celeste@foxgirl.tech>
2023-06-25 22:12:10 -0600

    ssh improvements maybe?

--- users/celeste/base/ssh.nix
+++ users/celeste/base/ssh.nix
@@ -4,8 +4,10 @@
     forwardAgent = true;
     forwardX11 = true;
     forwardX11Trusted = true;
-    # TODO: get this actually working? the presence of UIDs makes this difficult, though
-    extraOptions.RemoteForward = "/run/user/1111/gnupg/S.gpg-agent /run/user/1111/gnupg/S.gpg-agent.extra";
+    # TODO: get this actually working? this is difficult since I need to know where the remote host's `gpg` expects the socket, in the config here
+    # This is mostly obvious when I have, as i do in practice, different UIDs - I need to know what the UID is on the server statically, really, here?
+    # Going to try using %i, locally, and hardcode remote to 1000 - it's correct for star, and the default value...
+    extraOptions.RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/%i/gnupg/S.gpg-agent.extra";
   };
 in
 {
@@ -20,6 +22,7 @@ in
       "star.wg" = personalHost // { hostname = "10.255.255.10"; };
       # good for testing auth
       "github.com".user = "git";
+      "gitlab.com".user = "git";
       # if sshing to localhost, it's a personal host
       # ideally maybe a username, but this forces loopback netrworking at worst
       "localhost" = personalHost;
