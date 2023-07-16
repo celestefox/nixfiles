747cc1069502072283385cee2298fa8ad08e85ce
Celeste <celeste@foxgirl.tech>
2023-07-15 18:28:40 -0600

    polybar: alejandra, add disabled bottom-left bar

--- users/celeste/gui/polybar.nix
+++ users/celeste/gui/polybar.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, ... }: with lib; {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; {
   services.polybar = {
     enable = true;
     package = pkgs.polybar.override {
@@ -10,36 +16,37 @@
     script = ''
       polybar left &
       polybar right &
+      #polybar bottom-left &
     '';
-    settings =
-      let
-        common_module = {
-          # dashed format needed here because otherwise you need a deep merge not //
-          format-foreground = "\${colors.fg}";
-          format-background = "\${colors.bg}";
-          label-padding = "1";
-          #label-margin = "10px";
+    settings = let
+      common_module = {
+        # dashed format needed here because otherwise you need a deep merge not //
+        format-foreground = "\${colors.fg}";
+        format-background = "\${colors.bg}";
+        label-padding = "1";
+        #label-margin = "10px";
+      };
+      commonbar = {
+        height = "16";
+        background = "\${colors.trans}";
+        foreground = "\${colors.fg}";
+        #radius.bottom = "3";
+        font = [
+          "FiraCode Nerd Font:style=Regular:size=8;2"
+          "EB Garamond:style=Regular"
+        ];
+        #override-redirect = true;
+        modules = {
+          left = "date cpu mem";
+          center = "i3";
+          right = "wlan file vol";
         };
-        commonbar = {
-          height = "16";
-          background = "\${colors.trans}";
-          foreground = "\${colors.fg}";
-          #radius.bottom = "3";
-          font = [
-            "FiraCode Nerd Font:style=Regular:size=8;2"
-            "EB Garamond:style=Regular"
-          ];
-          #override-redirect = true;
-          modules = {
-            left = "date cpu mem";
-            center = "i3";
-            right = "wlan file vol";
-          };
-        };
-      in
-      {
-        # bars
-        "bar/left" = commonbar // {
+      };
+    in {
+      # bars
+      "bar/left" =
+        commonbar
+        // {
           monitor = "HDMI-A-0";
           tray = {
             position = "right";
@@ -48,51 +55,64 @@
             #padding = "2";
           };
         };
-        "bar/right" = commonbar // {
+      "bar/right" =
+        commonbar
+        // {
           monitor = "DVI-D-0";
         };
-        # colors
-        "colors" = {
-          bg = "#0d0d1b";
-          fg = "#ccccce";
-          trans = "#00000000";
-          "color0" = "#282828";
-          "color1" = "#ca1444";
-          "color2" = "#789aba";
-          "color3" = "#b3879f";
-          "color4" = "#94469b";
-          "color5" = "#cb6fa1";
-          "color6" = "#fb6e93";
-          "color7" = "#cf98c1";
-          "color8" = "#98218e";
-          "color9" = "#cb515d";
-          "color10" = "#5a87b1";
-          "color11" = "#9c61ab";
-          "color12" = "#9a77b1";
-          "color13" = "#f2a297";
-          "color14" = "#f4436f";
-          "color15" = "#ebdbb2";
+      "bar/bottom-left" = recursiveUpdate commonbar {
+        monitor = "HDMI-A-0";
+        bottom = true;
+        modules = {
+          left = "window";
+          center = "";
+          right = "media";
         };
-        # modules
-        ## center
-        "module/i3" = {
-          type = "internal/i3";
-          pin-workspaces = true;
-          show-urgent = true;
-          #strip-wsnumbers = true;
-          format = {
-            foreground = "\${colors.fg}";
-            background = "\${colors.bg}";
-          };
-          label = {
-            focused = {
-              foreground = "\${colors.color4}";
-            };
-            urgent.foreground = "\${colors.color14}";
-          };
+      };
+      # colors
+      "colors" = {
+        bg = "#0d0d1b";
+        fg = "#ccccce";
+        trans = "#00000000";
+        "color0" = "#282828";
+        "color1" = "#ca1444";
+        "color2" = "#789aba";
+        "color3" = "#b3879f";
+        "color4" = "#94469b";
+        "color5" = "#cb6fa1";
+        "color6" = "#fb6e93";
+        "color7" = "#cf98c1";
+        "color8" = "#98218e";
+        "color9" = "#cb515d";
+        "color10" = "#5a87b1";
+        "color11" = "#9c61ab";
+        "color12" = "#9a77b1";
+        "color13" = "#f2a297";
+        "color14" = "#f4436f";
+        "color15" = "#ebdbb2";
+      };
+      # modules
+      ## center
+      "module/i3" = {
+        type = "internal/i3";
+        pin-workspaces = true;
+        show-urgent = true;
+        #strip-wsnumbers = true;
+        format = {
+          foreground = "\${colors.fg}";
+          background = "\${colors.bg}";
         };
-        ## left
-        "module/date" = common_module // {
+        label = {
+          focused = {
+            foreground = "\${colors.color4}";
+          };
+          urgent.foreground = "\${colors.color14}";
+        };
+      };
+      ## left
+      "module/date" =
+        common_module
+        // {
           type = "internal/date";
           #interval = 1;
           label = "%date% %time%";
@@ -100,14 +120,20 @@
           date-alt = "%A, %d %b %Y";
           time = "%H:%M:%S";
         };
-        "module/cpu" = common_module // {
+      "module/cpu" =
+        common_module
+        // {
           type = "internal/cpu";
         };
-        "module/mem" = common_module // {
+      "module/mem" =
+        common_module
+        // {
           type = "internal/memory";
         };
-        ## right
-        "module/wlan" = common_module // {
+      ## right
+      "module/wlan" =
+        common_module
+        // {
           type = "internal/network";
           interface.type = "wireless";
           #format.packetloss = "<label-connected> <animation-packetloss>";
@@ -123,23 +149,29 @@
             padding = 1;
           };
         };
-        "module/wg" = common_module // {
+      "module/wg" =
+        common_module
+        // {
           type = "internal/network";
           interface = "wgnet";
           label.connected = "%ifname% %local_ip%";
           label.disconnected = "%ifname% DOWN";
           unknown-as-up = true;
         };
-        "module/file" = common_module // {
+      "module/file" =
+        common_module
+        // {
           type = "internal/fs";
-          mount = [ "/" ];
+          mount = ["/"];
           format.mounted = {
             foreground = "\${colors.fg}";
             background = "\${colors.bg}";
             padding = 1;
           };
         };
-        "module/vol" = common_module // {
+      "module/vol" =
+        common_module
+        // {
           type = "internal/pulseaudio";
           label-volume = "󰕾 %percentage%%";
           label-volume-background = "\${colors.bg}";
@@ -152,11 +184,26 @@
             right = "${pkgs.pavucontrol}/bin/pavucontrol";
           };
         };
-        /*         "module/round-left" = {
-          type = "custom/text";
-          content = "";
-          content-foregound = commonbar.background;
-          }; */
+      "module/media" = recursiveUpdate common_module {
+        type = "custom/script";
+        exec = "${pkgs.playerctl}/bin/playerctl metadata --format '{{ playerName }}: {{ artist }} - {{ title }} {{ duration(position) }}|{{ duration(mpris:length) }}' --follow";
+        tail = true;
+        format-foreground = "\${colors.color6}";
+        click.left = "${pkgs.playerctl}/bin/playerctl play-pause";
       };
+      "module/window" = recursiveUpdate common_module {
+        type = "internal/xwindow";
+        format-foreground = "\${colors.color6}";
+        label.empty.text = "";
+        label.empty.foreground = "\${colors.color1}";
+      };
+      /*
+               "module/round-left" = {
+      type = "custom/text";
+      content = "";
+      content-foregound = commonbar.background;
+      };
+      */
+    };
   };
 }
