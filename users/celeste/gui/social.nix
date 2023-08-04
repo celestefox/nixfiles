43081b84de1ad0357350a1161cb38c8ae4f0b01c
Celeste <celeste@foxgirl.tech>
2023-08-03 23:25:55 -0600

    celeste/social: add zoom-us

--- users/celeste/gui/social.nix
+++ users/celeste/gui/social.nix
@@ -1,11 +1,16 @@
-{ lib, pkgs, ... }: with lib;
 {
+  lib,
+  pkgs,
+  ...
+}:
+with lib; {
   # General packages
   home.packages = with pkgs; [
     #discordUnstable
     discord-ptb
     tdesktop
     mumble
+    zoom-us
     #teams # Microsoft Teams...
     teams-for-linux
   ];
