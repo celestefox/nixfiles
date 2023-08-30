199e7bed6bf6d181d5c58032e48caca83058173b
Celeste <celeste@foxgirl.tech>
2023-08-30 13:33:40 -0600

    celeste/konawall: add a couple helpers as .desktop

--- users/celeste/gui/konawall.nix
+++ users/celeste/gui/konawall.nix
@@ -1,7 +1,25 @@
-{ config, lib, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (config.systemd.user) systemctlPath;
+  inherit (config.services.konawall) konashow;
+in {
   services.konawall = {
     enable = true;
     interval = "5m";
   };
-  home.packages = lib.singleton config.services.konawall.konashow;
+  home.packages = lib.singleton konashow;
+  xdg.desktopEntries = {
+    "konarotate" = {
+      name = "konawall rotate";
+      exec = "${systemctlPath} --user --no-block restart konawall-rotation.service";
+    };
+    "konashow" = {
+      name = "konawall show";
+      exec = ''kitty --class kittyhere bash -c "konashow | less -+FS"'';
+    };
+  };
 }
