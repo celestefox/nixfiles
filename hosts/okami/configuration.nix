325ebea594be5eadb4090659b659f6db681c0a76
Celeste <celeste@foxgirl.tech>
2023-08-30 07:20:13 -0600

    treewide: fonts.fonts -> fonts.packages
    includes formatting of okami/configuration

--- hosts/okami/configuration.nix
+++ hosts/okami/configuration.nix
@@ -1,4 +1,13 @@
-{ lib, pkgs, config, inputs, users, profiles, ... }: with lib; {
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  users,
+  profiles,
+  ...
+}:
+with lib; {
   # Imports! mew
   imports = [
     # wsl
@@ -13,23 +22,32 @@
 
   # Locales (minimal)
   i18n.defaultLocale = "en_US.UTF-8"; # is default anyways but
-  i18n.supportedLocales = [ (config.i18n.defaultLocale + "/UTF-8") ];
+  i18n.supportedLocales = [(config.i18n.defaultLocale + "/UTF-8")];
 
   #users.users.youko.group = "users";
   #users.users.youko.isNormalUser = true;
 
   # Home manager
   home-manager = {
-    users.celeste = { imports = [ users.celeste.hm users.celeste.wsl /*users.celeste.gui*/ ]; };
+    users.celeste = {
+      imports = [
+        users.celeste.hm
+        users.celeste.wsl
+        /*
+        users.celeste.gui
+        */
+      ];
+    };
     #users.youko = { imports = singleton users.youko; };
-    /* beautiful... but I want only youko only here
-      users = mapAttrs (_: user: {
-      imports = singleton user;
-      }) users;
+    /*
+     beautiful... but I want only youko only here
+    users = mapAttrs (_: user: {
+    imports = singleton user;
+    }) users;
     */
   };
 
-  fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "FiraCode" "VictorMono" ]; }) ];
+  fonts.packages = [(pkgs.nerdfonts.override {fonts = ["FiraCode" "VictorMono"];})];
 
   # Fix up VSCode remote server
   services.vscode-server.enable = true;
