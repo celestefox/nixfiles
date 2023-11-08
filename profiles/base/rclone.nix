0693a7af11a80449c89034c287f3b1eca3f663ac
Celeste <celeste@foxgirl.tech>
2023-11-07 19:56:49 -0700

    turns out the mount.rclone symlink is packaged now

--- profiles/base/rclone.nix
+++ profiles/base/rclone.nix
@@ -5,14 +5,7 @@
 }:
 with lib; let
   inherit (pkgs) rclone;
-  rclone-mount = pkgs.symlinkJoin {
-    name = "rclone-mount";
-    paths = [rclone];
-    postBuild = ''
-      ln -s $out/bin/rclone $out/bin/mount.rclone
-    '';
-  };
 in {
-  environment.systemPackages = [rclone-mount];
-  system.fsPackages = [rclone-mount];
+  environment.systemPackages = [rclone];
+  system.fsPackages = [rclone];
 }
