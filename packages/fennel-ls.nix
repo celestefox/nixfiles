0d160001d85e157ef5c87bd38df8e6e553b32eac
Celeste <celeste@foxgirl.tech>
2023-11-19 22:59:34 -0700

    pkgs/fennel-ls: add meta, oop

--- packages/fennel-ls.nix
+++ packages/fennel-ls.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   fetchFromSourcehut,
   luajit,
 }:
@@ -15,4 +16,17 @@ luajit.pkgs.buildLuaPackage {
   patchPhase = ''
     patchShebangs --build fennel
   '';
+  meta = with lib; {
+    description = "A language server for Fennel";
+    longDescription = ''
+      A language server for fennel.
+      Supports Go-to-definition, and a little bit of completion suggestions.
+      Fennel-LS uses static analysis, and does not execute your code.
+
+      For now, you can ask fennel-ls to **treat your file as a macro file** if the very first characters in the file exactly match `;; fennel-ls: macro-file`. Expect this to change at some point in the future when I come up with a better way to specify which files are meant to be macro files.
+    '';
+    homepage = "https://git.sr.ht/~xerool/fennel-ls";
+    license = [licenses.mit];
+    mainProgram = "fennel-ls";
+  };
 }
