6a5a606093aeb82d73013c042a295e1d9947a2e9
Celeste <celeste@foxgirl.tech>
2023-12-18 19:24:03 -0700

    kdeconnect: add custom mpv helper shim
    also useful in other cases but envisioned to be launched by kde connect.

--- users/celeste/gui/kdeconnect.nix
+++ users/celeste/gui/kdeconnect.nix
@@ -1,6 +1,15 @@
-{ ... }: {
+{pkgs, ...}: {
   services.kdeconnect = {
     enable = true;
     indicator = true;
   };
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "kdeconnect-mpv";
+      runtimeInputs = [pkgs.xclip];
+      text = ''
+        /etc/profiles/per-user/celeste/bin/mpv --fs --fs-screen-name=DVI-D-0 "$(xclip -o -selection clipboard)"
+      '';
+    })
+  ];
 }
