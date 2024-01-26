5154a3cbae4d256df8704f90b930395f19047c91
Celeste <celeste@foxgirl.tech>
2024-01-26 10:01:47 -0700

    konawall on/off: simplification

--- users/celeste/gui/konawall.nix
+++ users/celeste/gui/konawall.nix
@@ -6,15 +6,23 @@
 }: let
   inherit (config.systemd.user) systemctlPath;
   inherit (config.services.konawall) konashow;
+  # i originally attempted to use masking in these, in hopes it could keep it
+  # disabled across switches, but i could tell it wouldn't work with a status,
+  # the masking wasn't visible. i believe this is because: the unit in practice
+  # is loaded from `~/.config/systemd/user/` for me, masking ended up in
+  # `/run/user/1111/systemd/user/` (or `$XDG_RUNTIME_DIR/systemd/user/`); as per
+  # systemctl(1) on the mask command you need to care about the unit search path
+  # - the mask must come first; systemd.unit(5) shows that in the "User Unit
+  # Search Path", the XDG_RUNTIME_DIR path is lower than even the /etc path
+  # which is lower than ~/.config. Interestingly, noting a similar situation
+  # with the system unit path that should make system masking useless too...
+  # does masking not actually work on nixos, effectively?
   konaoff = pkgs.writeShellScriptBin "konaoff" ''
     ${systemctlPath} --user is-active konawall-rotation.timer || exit 1
     ${systemctlPath} --user stop konawall-rotation.timer
-    ${systemctlPath} --user --runtime mask konawall-rotation.timer
     ${pkgs.feh}/bin/feh --no-fehbg --bg-scale ${config.stylix.image}
   '';
   konaon = pkgs.writeShellScriptBin "konaon" ''
-    if ${systemctlPath} --user is-enabled konawall-rotation.timer; then exit 1; fi
-    ${systemctlPath} --user --runtime unmask konawall-rotation.timer
     ${systemctlPath} --user start konawall-rotation.timer
   '';
 in {
