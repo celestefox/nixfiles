c5f35bec22bdcc3a7ba128acf8945accec68d006
Celeste <celeste@foxgirl.tech>
2024-02-20 01:29:44 -0700

    shell: make starship use nerd fonts symbols

--- users/celeste/base/shell.nix
+++ users/celeste/base/shell.nix
@@ -138,7 +138,10 @@ with lib; {
   programs.starship = {
     enable = true;
     settings = {
-      aws.disabled = true;
+      aws = {
+        disabled = true;
+        symbol = "  ";
+      };
       shell = {
         disabled = false;
         # don't display anything for fish, only other shells
@@ -148,8 +151,165 @@ with lib; {
       };
       nix_shell = {
         #heuristic = true;
+        symbol = " ";
       };
     };
+    extraConfig = ''
+      [buf]
+      symbol = " "
+
+      [c]
+      symbol = " "
+
+      [conda]
+      symbol = " "
+
+      [crystal]
+      symbol = " "
+
+      [dart]
+      symbol = " "
+
+      [directory]
+      read_only = " 󰌾"
+
+      [docker_context]
+      symbol = " "
+
+      [elixir]
+      symbol = " "
+
+      [elm]
+      symbol = " "
+
+      [fennel]
+      symbol = " "
+
+      [fossil_branch]
+      symbol = " "
+
+      [git_branch]
+      symbol = " "
+
+      [golang]
+      symbol = " "
+
+      [guix_shell]
+      symbol = " "
+
+      [haskell]
+      symbol = " "
+
+      [haxe]
+      symbol = " "
+
+      [hg_branch]
+      symbol = " "
+
+      [hostname]
+      ssh_symbol = " "
+
+      [java]
+      symbol = " "
+
+      [julia]
+      symbol = " "
+
+      [kotlin]
+      symbol = " "
+
+      [lua]
+      symbol = " "
+
+      [memory_usage]
+      symbol = "󰍛 "
+
+      [meson]
+      symbol = "󰔷 "
+
+      [nim]
+      symbol = "󰆥 "
+
+      [nodejs]
+      symbol = " "
+
+      [ocaml]
+      symbol = " "
+
+      [os.symbols]
+      Alpaquita = " "
+      Alpine = " "
+      Amazon = " "
+      Android = " "
+      Arch = " "
+      Artix = " "
+      CentOS = " "
+      Debian = " "
+      DragonFly = " "
+      Emscripten = " "
+      EndeavourOS = " "
+      Fedora = " "
+      FreeBSD = " "
+      Garuda = "󰛓 "
+      Gentoo = " "
+      HardenedBSD = "󰞌 "
+      Illumos = "󰈸 "
+      Linux = " "
+      Mabox = " "
+      Macos = " "
+      Manjaro = " "
+      Mariner = " "
+      MidnightBSD = " "
+      Mint = " "
+      NetBSD = " "
+      NixOS = " "
+      OpenBSD = "󰈺 "
+      openSUSE = " "
+      OracleLinux = "󰌷 "
+      Pop = " "
+      Raspbian = " "
+      Redhat = " "
+      RedHatEnterprise = " "
+      Redox = "󰀘 "
+      Solus = "󰠳 "
+      SUSE = " "
+      Ubuntu = " "
+      Unknown = " "
+      Windows = "󰍲 "
+
+      [package]
+      symbol = "󰏗 "
+
+      [perl]
+      symbol = " "
+
+      [php]
+      symbol = " "
+
+      [pijul_channel]
+      symbol = " "
+
+      [python]
+      symbol = " "
+
+      [rlang]
+      symbol = "󰟔 "
+
+      [ruby]
+      symbol = " "
+
+      [rust]
+      symbol = " "
+
+      [scala]
+      symbol = " "
+
+      [swift]
+      symbol = " "
+
+      [zig]
+      symbol = " "
+    '';
   };
   programs.nix-index.enable = true;
 
