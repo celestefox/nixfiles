be431fdeeb7d09c8f490f5d5504cff1b5a1c7971
Celeste <celeste@foxgirl.tech>
2024-02-20 02:23:29 -0700

    neovim: ibl rainbow highlighting

--- users/celeste/base/neovim.nix
+++ users/celeste/base/neovim.nix
@@ -72,9 +72,39 @@
       {
         plugin = indent-blankline-nvim;
         type = "lua";
-        config = ''
-          require'ibl'.setup{}
-        '';
+        config =
+          /*
+          lua
+          */
+          ''
+            local rainbowhl = {
+              "RainbowRed",
+              "RainbowOrange",
+              "RainbowYellow",
+              "RainbowGreen",
+              "RainbowCyan",
+              "RainbowBlue",
+              "RainbowViolet",
+            }
+
+            local iblhooks = require'ibl.hooks'
+
+            iblhooks.register(iblhooks.type.HIGHLIGHT_SETUP, function()
+              vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+              vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+              vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+              vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+              vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+              vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+              vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            require'ibl'.setup{
+              indent = {
+                highlight = rainbowhl,
+              },
+            }
+          '';
       }
       vim-eunuch
       zoxide-vim
