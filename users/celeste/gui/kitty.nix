1263ef184aea17781b544bf78b1655ad73330c18
Celeste <celeste@foxgirl.tech>
2024-03-19 04:17:37 -0600

    kitty: actually fully complete symbol_map i hadn't gotten everything, including the character actually in 3.1.1, embarassingly which i think neovim was also using, too, how i noticed this

--- users/celeste/gui/kitty.nix
+++ users/celeste/gui/kitty.nix
@@ -81,7 +81,7 @@ in {
       extraConfig = ''
         modify_font underline_position 2
         # originally for v2.3.3, updated to v3.1.1 by celeste
-        symbol_map U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F0001-U+F1AF0 Symbols Nerd Font Mono
+        symbol_map U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6B2,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F372,U+F400-U+F532,U+F0001-U+F1AF0 Symbols Nerd Font Mono
       '';
       sessions = {
         status = {
